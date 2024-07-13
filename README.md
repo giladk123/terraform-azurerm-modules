830a06dba8417496137edec51884630b0d36846f
giladk123 <giladk@gmail.com>
2024-07-13 15:23:43 +0300

    new modules bundle repo

---
+++ README.md
@@ -0,0 +1,5 @@
+## Usage
+
+This is a bundle of modules for azurerm providers
+
+to use each of the submodules reffer to the readme file on each submodule
---
+++ modules/resource-group/variable.tf
@@ -0,0 +1,7 @@
+variable "resource_groups" {
+  description = "Map of resource group details"
+  type = map(object({
+    rg_location = string
+    rg_tags     = map(string)
+  }))
+}
