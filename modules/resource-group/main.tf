5e9d1454b03965b2e8dc02cd30cbf5729542f6e4
giladk123 <83569702+giladk123@users.noreply.github.com>
2024-12-21 17:39:57 +0200

    name convention to resource group

--- modules/resource-group/main.tf
+++ modules/resource-group/main.tf
@@ -10,7 +10,7 @@ terraform {
 resource "azurerm_resource_group" "rg" {
   for_each = var.resource_groups
 
-  name     = each.key
+  name     = "${local.name_prefix}-${each.key}-rg"
   location = each.value.rg_location
   tags     = each.value.rg_tags
 }
--- modules/resource-group/variable.tf
+++ modules/resource-group/variable.tf
@@ -5,3 +5,14 @@ variable "resource_groups" {
     rg_tags     = map(string)
   }))
 }
+
+variable "name_convention" {
+  description = "Naming convention details."
+  type = object({
+    region                    = string
+    dbank_idbank_first_letter = string
+    env                       = string
+    cmdb_infra                = string
+    cmdb_project              = string
+  })
+}
