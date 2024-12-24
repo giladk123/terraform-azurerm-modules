760896e42b86361455ee983e24c5b41b217c9cfb
giladk123 <83569702+giladk123@users.noreply.github.com>
2024-12-24 15:50:17 +0200

    changed vm module

--- modules/virtual-machine/outputs.tf
+++ modules/virtual-machine/outputs.tf
@@ -21,11 +21,6 @@ output "network_interface_ids" {
   value       = { for k, v in azurerm_network_interface.nic : k => v.id }
 }
 
-output "public_ip_dns_names" {
-  description = "DNS names of the public IPs."
-  value       = { for k, v in azurerm_public_ip.pip : k => v.dns_settings[0].domain_name_label }
-}
-
 output "nsg_associations" {
   description = "Network Security Group Associations of the virtual machines."
   value       = { for k, v in azurerm_network_interface_security_group_association.nsg_association : k => v.id }
