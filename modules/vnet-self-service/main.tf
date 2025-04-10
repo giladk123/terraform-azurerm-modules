6fbefb3683e3867cc4d85bdd825f24e4920e1904
Gilad Kalev <giladk@gmail.com>
2025-04-10 09:09:22 +0300

    created root module for vnet self service

---
+++ modules/vnet-self-service/main.tf
@@ -0,0 +1,25 @@
+resource "azurerm_virtual_network" "vnet" {
+  for_each            = var.vnet_config
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+  address_space       = each.value.address_space
+}
+
+resource "azurerm_subnet" "subnet" {
+  for_each             = flatten([
+    for vnet_key, vnet in var.vnet_config : [
+      for subnet_key, subnet in vnet.subnets : {
+        vnet_key      = vnet_key
+        subnet_key    = subnet_key
+        subnet_config = subnet
+        vnet_name    = azurerm_virtual_network.vnet[vnet_key].name
+        rg_name      = vnet.resource_group_name
+      }
+    ]
+  ])
+  name                 = each.value.subnet_config.name
+  resource_group_name  = each.value.rg_name
+  virtual_network_name = each.value.vnet_name
+  address_prefixes     = each.value.subnet_config.address_prefixes
+}
---
+++ modules/vnet-self-service/variables.tf
@@ -0,0 +1,13 @@
+variable "vnet_config" {
+  description = "Configuration for the VNet and its subnets"
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    location           = string
+    address_space      = list(string)
+    subnets = map(object({
+      name             = string
+      address_prefixes = list(string)
+    }))
+  }))
+}
