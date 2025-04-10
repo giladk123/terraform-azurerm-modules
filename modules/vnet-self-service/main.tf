560cf5faa6f452c45fee5a48417d9e3d292abf01
Gilad Kalev <giladk@gmail.com>
2025-04-10 09:37:19 +0300

    created root module for vnet self service

--- modules/vnet-self-service/main.tf
+++ modules/vnet-self-service/main.tf
@@ -7,17 +7,21 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  for_each             = flatten([
-    for vnet_key, vnet in var.vnet_config : [
-      for subnet_key, subnet in vnet.subnets : {
-        vnet_key      = vnet_key
-        subnet_key    = subnet_key
-        subnet_config = subnet
-        vnet_name    = azurerm_virtual_network.vnet[vnet_key].name
-        rg_name      = vnet.resource_group_name
-      }
-    ]
-  ])
+  for_each = {
+    for subnet in flatten([
+      for vnet_key, vnet in var.vnet_config : [
+        for subnet_key, subnet in vnet.subnets : {
+          id           = "${vnet_key}-${subnet_key}"
+          vnet_key    = vnet_key
+          subnet_key  = subnet_key
+          subnet_config = subnet
+          vnet_name   = azurerm_virtual_network.vnet[vnet_key].name
+          rg_name     = vnet.resource_group_name
+        }
+      ]
+    ]) : subnet.id => subnet
+  }
+  
   name                 = each.value.subnet_config.name
   resource_group_name  = each.value.rg_name
   virtual_network_name = each.value.vnet_name
