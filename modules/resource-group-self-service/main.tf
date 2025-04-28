5615d3bf6a8b873edb57970771b189764664859a
Gilad Kalev <giladk@gmail.com>
2025-04-28 22:34:37 +0300

    update

--- modules/resource-group-self-service/main.tf
+++ modules/resource-group-self-service/main.tf
@@ -1,15 +1,12 @@
 locals {
   resource_groups = var.resource_groups
-  unique_subscriptions = distinct([for rg in local.resource_groups : rg.subscription_id])
 }
 
-# Create resource groups with their respective subscription IDs
+# Create resource groups
 resource "azurerm_resource_group" "this" {
   for_each = local.resource_groups
 
   name     = "${each.value.name_convention.region}-${each.value.name_convention.dbank_idbank_first_letter}-${each.value.name_convention.env}-${each.value.name_convention.cmdb_infra}-${each.value.name_convention.cmdb_project}-${each.key}-rg"
   location = each.value.rg_location
   tags     = each.value.rg_tags
-
-  provider = azurerm.aliases
 } 
