aa4bcb6629bfe68b2bfab4b8d4d05ee84f055182
Gilad Kalev <giladk@gmail.com>
2025-04-29 12:45:19 +0300

    update

--- modules/resource-group-self-service/main.tf
+++ modules/resource-group-self-service/main.tf
@@ -6,7 +6,7 @@ locals {
 resource "azurerm_resource_group" "this" {
   for_each = local.resource_groups
 
-  name     = "${each.value.name_convention.region}-${each.value.name_convention.dbank_idbank_first_letter}-${each.value.name_convention.env}-${each.value.name_convention.cmdb_infra}-${each.value.name_convention.cmdb_project}-${each.key}-rg"
+  name     = "${each.value.name_convention.region}-${each.value.name_convention.dbank_idbank_first_letter}${each.value.name_convention.env}-${each.value.name_convention.cmdb_infra}-${each.value.name_convention.cmdb_project}-${each.key}-rg"
   location = each.value.rg_location
   tags     = each.value.rg_tags
 } 
