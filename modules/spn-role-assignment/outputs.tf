f91f8684ac8b858cfb43557dc45d4cbd4ba51c51
Gilad Kalev <giladk@gmail.com>
2025-05-14 11:02:52 +0300

    update

--- modules/spn-role-assignment/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "role_assignment_ids" {
-  description = "Map of role assignment names to their IDs"
-  value       = { for k, v in azurerm_role_assignment.spn_role_assignment : k => v.id }
-}
