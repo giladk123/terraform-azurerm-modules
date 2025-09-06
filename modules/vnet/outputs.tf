9d0965429e287d2e073cbd58dd1dd6f427ad8911
Gilad Kalev <giladk@gmail.com>
2025-09-06 19:33:12 +0300

    init module AKS

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -3,7 +3,19 @@ output "vnet" {
   value       = azurerm_virtual_network.vnet
 }
 
+output "vnets" {
+  description = "All virtual networks (alias for compatibility)"
+  value       = azurerm_virtual_network.vnet
+}
+
 output "subnet" {
   description = "All subnets"
   value       = azurerm_subnet.subnet
+}
+
+output "subnet_ids" {
+  description = "Map of subnet names to their IDs"
+  value       = {
+    for key, subnet in azurerm_subnet.subnet : key => subnet.id
+  }
 }
